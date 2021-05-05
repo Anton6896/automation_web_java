d21e4113a976735f5a421fe75f5e9f5d3f29e7d4
mainPC <anton-r@live.com>
2021-05-05 19:29:52 +0300

    os auto check

--- src/webElements/WebElements.java
+++ src/webElements/WebElements.java
@@ -8,12 +8,25 @@ import java.io.File;
 
 public class WebElements {
 
-    static String driverPath = "drivers_for_selenium" + File.separator +
-            "chromedriver_win32" + File.separator +
-            "chromedriver.exe";
+    static String driverPath = null;
     static WebDriver driver;
+    private static String OS = null;
+
+    static void mySys() {
+        if (OS == null) {
+            OS = System.getProperty("os.name");
+        }
+
+        if ("Windows 10".equals(OS)) {
+            driverPath = "drivers_for_selenium" + File.separator +
+                    "chromedriver_win32" + File.separator +
+                    "chromedriver.exe";
+        }
+
+    }
 
     public static void main(String[] args) {
+        mySys();
         System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver();
 
@@ -22,18 +35,23 @@ public class WebElements {
         driver.findElement(By.id("user-message")).sendKeys("testing");
         driver.findElement(By.cssSelector("button.btn:nth-child(2)")).click();
         String txt1 = driver.findElement(By.id("display")).getText();
-        System.out.println("text is : " + txt1);
-
+        if (txt1.equals("testing")) {
+            System.out.println("test 1 ok");
+        }
 
 //        check box
         driver.findElement(By.cssSelector("ul.nav:nth-child(1) > li:nth-child(1) > a:nth-child(1)")).click();
         driver.findElement(By.cssSelector(".open > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)")).click();
         driver.findElement(By.id("isAgeSelected")).click();
         String txt2 = driver.findElement(By.id("txtAge")).getText();
-        System.out.println(txt2);
-
+        if (txt2.split(" ")[0].equals("Success")) {
+            System.out.println("test 2 ok");
+        }
 
 
         driver.close();
+
     }
+
+
 }
